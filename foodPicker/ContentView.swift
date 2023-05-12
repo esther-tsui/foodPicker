6cc152785004bad5a29cea18784b05fb6a5eaed6
Esther Tsui <66723850+esther-tsui@users.noreply.github.com>
2023-05-12 17:36:00 +0100

    first commit

---
+++ foodPicker/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  foodPicker
+//
+//  Created by Wing Hei Tsui on 12/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    let food = ["漢堡", "沙拉", "披薩", "義大利麵", "雞腿便當", "刀削麵", "火鍋", "牛肉麵", "關東煮"]
+    @State private var selectedFood: String?
+    
+    var body: some View {
+        VStack(spacing: 30) {
+            Image("dinner")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                
+            Text("Waht to eat today?")
+                .font(.title)
+                .bold()
+            
+            if selectedFood != .none {
+                Text(selectedFood ?? "")
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+            }
+
+
+            Button("Tell me!") {
+                selectedFood = food.shuffled().first
+            }.font(.title)
+                .buttonStyle(.borderedProminent)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ foodPicker/foodPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodPickerApp.swift
+//  foodPicker
+//
+//  Created by Wing Hei Tsui on 12/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct foodPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
